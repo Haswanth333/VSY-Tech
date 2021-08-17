c0846f4621e0a82bed184028c1a3ca9ad0960154
Haswanth333 <haswanthroyal333@gmail.com>
2021-08-17 13:08:42 +0530

    Signup complete

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,18 +3,23 @@ const express = require('express')
 
 const router = express.Router()
 const signUpTemplateCopy = require('../models/SignupModels')
+const bcrypt = require('bcrypt')
 
-router.post('/signup', (request,response) => {
+router.post('/signup', async (request,response) => {
+
+    const saltPassword = await bcrypt.genSalt(10)
+
+    const securePassword = await bcrypt.hash(request.body.password, saltPassword)
     // response.send('send')
     const signUpUser = new signUpTemplateCopy({
         fullName:request.body.fullName,
         username:request.body.username,
         email:request.body.email,
-        password:request.body.password
+        password:securePassword
     })
     signUpUser.save()
     .then(data => {
-        response,jason(data)
+        response.json(data)
     })
     .catch(error => {
         response.json(error)
