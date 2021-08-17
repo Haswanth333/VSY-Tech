a8b6d36ddb1d2ff6041c9d38b643ad24c25badc0
Haswanth333 <haswanthroyal333@gmail.com>
2021-08-17 09:57:53 +0530

    Server/ Routing/ Schema

---
+++ backend/routes/routes.js
@@ -0,0 +1,25 @@
+// const { response } = require('express')
+const express = require('express')
+
+const router = express.Router()
+const signUpTemplateCopy = require('../models/SignupModels')
+
+router.post('/signup', (request,response) => {
+    // response.send('send')
+    const signUpUser = new signUpTemplateCopy({
+        fullName:request.body.fullName,
+        username:request.body.username,
+        email:request.body.email,
+        password:request.body.password
+    })
+    signUpUser.save()
+    .then(data => {
+        response,jason(data)
+    })
+    .catch(error => {
+        response.json(error)
+    })
+
+})
+
+module.exports = router
